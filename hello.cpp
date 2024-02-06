92aec6f8a8942afdb195113491572cb7a689634a
John Salako <salakojo@msu.edu>
2024-02-05 22:28:21 -0500

    Part 3: MPI basics with helloworld

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,17 @@
+#include "mpi.h"
 #include <iostream>
 using namespace std;
 
-int main() 
+int main(int argc, char *argv[]) 
 {
-    cout << "Hello, World!" << endl;
+    int numtasks, rank;  // it is important to initialize variables in the main function
+    MPI_Init(&argc, &argv);  // ensure that the argc and argv are declared also in the main fuction
+    
+    MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    if (rank == numtasks-1){
+        cout << "Hello, World!" << endl;
+    }
+    MPI_Finalize();
     return 0;
 }
